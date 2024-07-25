d039487aa6e29a35cab9882a9ce75ff488121781
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-07-25 14:43:29 +0700

    perubahan di database

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "srv596.hstgr.io",
-  USER: "u610515881_ngurusizin",
-  PASSWORD: "NgurusIzin123#",
-  DB: "u610515881_ngurusizin",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "gmt_soft_development",
   dialect: "mysql",
   pool: {
     max: 5,
