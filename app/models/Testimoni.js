6a5bb5887570258f6615286f463bbf5e8920747b
jipyy <jahdanpandita16@gmail.com>
2024-07-30 13:23:33 +0700

    testimoni

--- app/models/Testimoni.js
+++ app/models/Testimoni.js
@@ -1,22 +1,18 @@
-
 module.exports = (sequelize, Sequelize) => {
-    const Testimoni = sequelize.define("testimoni", {
-      nama: {
-        type: Sequelize.STRING
-      },
-      gambar: {
-        type: Sequelize.STRING
-      },
-      urlGambar: {
-        type: Sequelize.STRING
-      },
-      testimoni: {
-        type: Sequelize.STRING
-      },
-      jabatan: {
-        type: Sequelize.STRING
-      },
-    });
+  const Testimoni = sequelize.define("testimoni", {
+    jenis_testimoni: {
+      type: Sequelize.ENUM('wa', 'email'), // Enum untuk tipe testimoni
+    },
+    gambar_testimoni: {
+      type: Sequelize.STRING, // URL atau path gambar
+    },
+    judul_testimoni: {
+      type: Sequelize.STRING, // Judul testimoni
+    },
+    deskripsi_testimoni: {
+      type: Sequelize.STRING, // Deskripsi testimoni
+    },
+  });
 
-    return Testimoni;
-  };
+  return Testimoni;
+};
--- app/routes/setting.js
+++ app/routes/setting.js
@@ -3,11 +3,12 @@
   module.exports = (app) => {
     const router = require("express").Router();
     const setting = require("../controllers/settingContoller");;
+    const upl_setting = require("../middleware/setting")
   
-    router.post("/", setting.create);
+    router.post("/", upl_setting.single("foto"),setting.create);
     router.get("/", setting.findAll);
-    router.get("/:id", setting.findOne);
-    router.put("/:id", setting.update);
+    router.get("/:id", upl_setting.single("foto"), setting.findOne);
+    router.patch("/:id", upl_setting.single("foto"), setting.update);
     router.delete("/:id", setting.delete);
     router.delete("/", setting.deleteAll);
   
