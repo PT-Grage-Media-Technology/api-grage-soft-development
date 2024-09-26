45ea47d324db8349f8309c0fc3d82e6e03ca7b27
ahmd-fariz <ahmadfarizmaulana4@gmail.com>
2024-09-26 10:32:00 +0700

    api config

--- app/configs/database.js
+++ app/configs/database.js
@@ -12,3 +12,18 @@ module.exports = {
   },
   JWT_SECRET: "izinaaja",
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "gmt_soft_development",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+//   JWT_SECRET: "izinaaja",
+// };
